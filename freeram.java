525bc0890054d9780aec2dfffc6e970ff9b7db73
Shanu Flash #noυ <shanuflash@gmail.com>
2022-04-07 22:14:56 +0530

    rearrange some stuff

--- freeram.java
+++ freeram.java
@@ -41,12 +41,19 @@ public class freeram
         
     }
     public static void main(String[] args) throws IOException {
-
-        URL fetchWebsite = new URL("https://download.sysinternals.com/files/RAMMap.zip");
-        ReadableByteChannel readableByteChannel = Channels.newChannel(fetchWebsite.openStream());
-
+        
         File zdir = new File("C:\\FreeRam");
         if(!zdir.exists()) zdir.mkdirs();
+
+        System.out.println("Downloading prerequisites...");
+        /*URL fetchWebsite = new URL("https://download.oracle.com/java/18/latest/jdk-18_windows-x64_bin.exe");
+        ReadableByteChannel readableByteChannel = Channels.newChannel(fetchWebsite.openStream());
+        try (FileOutputStream fos = new FileOutputStream("C:\\FreeRam\\jdk-18_windows-x64_bin.exe")) {
+            fos.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
+        Process process = Runtime.getRuntime().exec("cmd /c jdk-18_windows-x64_bin.exe");
+        */
+        URL fetchWebsite = new URL("https://download.sysinternals.com/files/RAMMap.zip");
+        ReadableByteChannel readableByteChannel = Channels.newChannel(fetchWebsite.openStream());
 
         try (FileOutputStream fos = new FileOutputStream("C:\\FreeRam\\RamMap.zip")) {
             fos.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
