89c1af04e606b98dfbfeff8ea7969b77e9182592
Shanu Flash #noυ <shanuflash@gmail.com>
2022-04-07 20:43:55 +0530

    Download rammap

--- freeram.java
+++ freeram.java
@@ -1,1 +1,13 @@
+import java.net.*;
+import java.nio.channels.*;
+import java.io.*;
 
+public class freeram 
+{    public static void main(String[] args) throws IOException {
+
+        URL fetchWebsite = new URL("https://download.sysinternals.com/files/RAMMap.zip");
+        ReadableByteChannel readableByteChannel = Channels.newChannel(fetchWebsite.openStream());
+        try (FileOutputStream fos = new FileOutputStream("C:\\Users\\shanu\\Downloads\\RamMap.zip")) {
+            fos.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
+        }
+}
