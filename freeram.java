29ab788255302943090692979e2da37f6512dec1
Shanu Flash #noυ <shanuflash@gmail.com>
2022-04-07 20:56:42 +0530

    Error handling and finish unzip function

--- freeram.java
+++ freeram.java
@@ -1,10 +1,11 @@
 import java.net.*;
 import java.nio.channels.*;
 import java.io.*;
+import java.util.zip.*;
 
 public class freeram 
 {    
-        private static void unzip(String Path, String Dir) {
+    private static void unzip(String Path, String Dir) {
         File dir = new File(Dir);
         if(!dir.exists()) dir.mkdirs();
         FileInputStream fis;
@@ -33,14 +34,26 @@ public class freeram
             zis.close();
             fis.close();
         } 
-                
-        public static void main(String[] args) throws IOException {
-                URL fetchWebsite = new URL("https://download.sysinternals.com/files/RAMMap.zip");
-                ReadableByteChannel readableByteChannel = Channels.newChannel(fetchWebsite.openStream());
         
-                File zdir = new File("C:\\FreeRam");
-                if(!zdir.exists()) zdir.mkdirs();
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+    }
+    public static void main(String[] args) throws IOException {
 
-                try (FileOutputStream fos = new FileOutputStream("C:\\FreeRam\\RamMap.zip")) {
-                    fos.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
+        URL fetchWebsite = new URL("https://download.sysinternals.com/files/RAMMap.zip");
+        ReadableByteChannel readableByteChannel = Channels.newChannel(fetchWebsite.openStream());
+
+        File zdir = new File("C:\\FreeRam");
+        if(!zdir.exists()) zdir.mkdirs();
+
+        try (FileOutputStream fos = new FileOutputStream("C:\\FreeRam\\RamMap.zip")) {
+            fos.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
+ 
+        String Path = "C:\\FreeRam\\RamMap.zip";
+        String Dir = "C:\\FreeRam"; 
+        unzip(Path, Dir);
+        }
+    }
 }
