403e055f44c6ee47cd9a1bb364073c841e35c78a
Shanu Flash #noυ <shanuflash@gmail.com>
2022-04-07 20:40:26 +0530

    F

--- freeram.bat
+++
@@ -1,8 +0,0 @@
-move "data\freeram.vbs" "%USERPROFILE%\Start Menu\Programs\Startup"
-move "freeram.exe" "%USERPROFILE%\Start Menu\Programs\Startup"
-move "data\RAMMap.exe" C:\Windows
-@echo off
-for /l %%i in (1, 1, 11) do (
-	rammap -ew
-	timeout /t 3600 /nobreak
-)
