979b6a5b1c54f5a2e24fce58c32028eb6e687546
Shanu Flash #noυ <shanuflash@gmail.com>
2022-04-07 20:48:32 +0530

    unzip

--- freeram.java
+++ freeram.java
@@ -3,14 +3,44 @@ import java.nio.channels.*;
 import java.io.*;
 
 public class freeram 
-{    public static void main(String[] args) throws IOException {
+{    
+        private static void unzip(String Path, String Dir) {
+        File dir = new File(Dir);
+        if(!dir.exists()) dir.mkdirs();
+        FileInputStream fis;
+        byte[] buffer = new byte[1024];
+        try 
+        {
+            fis = new FileInputStream(Path);
+            ZipInputStream zis = new ZipInputStream(fis);
+            ZipEntry ze = zis.getNextEntry();
+            while(ze != null)
+            {
+                String fileName = ze.getName();
+                File newFile = new File(Dir + File+ fileName);
+                System.out.println("Unzipping to "+newFile.getAbsolutePath());
+                new File(newFile.getParent()).mkdirs();
+                FileOutputStream fos = new FileOutputStream(newFile);
+                int len;
+                while ((len = zis.read(buffer)) > 0){
+                    fos.write(buffer, 0, len);
+                }
+                fos.close();
+                zis.closeEntry();
+                ze = zis.getNextEntry();
+            }
+            zis.closeEntry();
+            zis.close();
+            fis.close();
+        } 
+                
+        public static void main(String[] args) throws IOException {
+                URL fetchWebsite = new URL("https://download.sysinternals.com/files/RAMMap.zip");
+                ReadableByteChannel readableByteChannel = Channels.newChannel(fetchWebsite.openStream());
+        
+                File zdir = new File("C:\\FreeRam");
+                if(!zdir.exists()) zdir.mkdirs();
 
-        URL fetchWebsite = new URL("https://download.sysinternals.com/files/RAMMap.zip");
-        ReadableByteChannel readableByteChannel = Channels.newChannel(fetchWebsite.openStream());
-
-        File zdir = new File("C:\\FreeRam");
-        if(!zdir.exists()) zdir.mkdirs();
-
-        try (FileOutputStream fos = new FileOutputStream("C:\\FreeRam\\RamMap.zip")) {
-            fos.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
+                try (FileOutputStream fos = new FileOutputStream("C:\\FreeRam\\RamMap.zip")) {
+                    fos.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
 }
