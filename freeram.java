8d89143861efbc43b6bd70283556f628d4bb7e08
Shanu Flash #noυ <shanuflash@gmail.com>
2022-04-18 21:50:14 +0530

    make UI better, 'change timings' is still WIP

--- freeram.java
+++ freeram.java
@@ -4,7 +4,6 @@ import java.io. *;
 import java.util.zip. *;
 import javax.swing. *;
 import java.awt.event. *;
-import java.awt.SystemTray;
 import java.awt. *;
 import java.lang.Thread;
 
@@ -87,14 +86,51 @@ public class freeram {
         unzip(Path, Dir);
 
         //  Swing  UI
+        //  Frame
         JFrame f = new JFrame("Free Ram");
-        JButton b = new JButton("Click Here");
-        // JTextField t = new JTextField("Test"); t.setBounds(10, 10, 100, 100);
-        // t.addActionListener(new ActionListener() {     try {         System
-        // .out             .println("test");     } catch (IOException tt) {
-        // tt.printStackTrace();     } });
-        b.setBounds(65, 14, 100, 30);
-        b.addActionListener(new ActionListener() {
+        f.setSize(315, 200);
+        f.setLayout(null);
+        ImageIcon img = new ImageIcon("bin\\ram.png");
+        f.setIconImage(img.getImage());
+        f.setLocationRelativeTo(null);
+        f.setVisible(true);
+
+        //  Label l1
+        JLabel l1 = new JLabel("Change Timings: ");
+        Font font1 = new Font("Roboto", Font.PLAIN, 20);
+        l1.setFont(font1);
+        //System.out.println("L1= " + l1.getPreferredSize());
+        l1.setBounds(20,20, 152, 26);  
+        f.add(l1); 
+
+        //  TextField t1
+        JTextField t1 = new JTextField(" 60");
+        t1.setBounds(20, 55, 250, 35);
+        Font font2 = new Font("Roboto", Font.PLAIN, 17);
+        t1.setFont(font2);
+        t1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String timestr = t1.getText();
+                double time = Double.parseDouble(timestr);
+                System.out.println(time);
+            }
+        });
+        f.add(t1);
+
+        //  Label l2
+        JLabel l2 = new JLabel("Manual Clean:");
+        //Font font2 = new Font("Roboto", Font.PLAIN, 20);
+        //System.out.println("L2= " + l2.getPreferredSize());
+        l2.setFont(font1);
+        l2.setBounds(20,110, 125, 26);  
+        f.add(l2);
+
+        //  Button b1
+        JButton b1 = new JButton("Click Here");
+        b1.setBounds(150, 110, 100, 30);
+        b1.setFocusPainted(false);
+        b1.setContentAreaFilled(false);
+        b1.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 try {
                     Runtime
@@ -105,15 +141,9 @@ public class freeram {
                 }
             }
         });
-        // f.add(t);
-        f.add(b);
-        f.setSize(245, 100);
-        f.setLayout(null);
-        ImageIcon img = new ImageIcon("bin\\ram.png");
-        f.setIconImage(img.getImage());
-        f.setLocationRelativeTo(null);
-        f.setVisible(true);
+        f.add(b1);
 
+        
         //  System  Tray
         if (!SystemTray.isSupported()) {
             System
