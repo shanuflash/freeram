ec23bda02290f1a2d8508375c5ea4a6e2dd1410f
Shanu Flash #noυ <shanuflash@gmail.com>
2022-04-27 14:14:23 +0530

    remove comment

--- etc/download.java
+++ etc/download.java
@@ -11,14 +11,7 @@ public class download {
         System
             .out
             .println("Downloading prerequisites...");
-
-        /*URL fetchWebsite = new URL("https://download.oracle.com/java/18/latest/jdk-18_windows-x64_bin.exe");
-        ReadableByteChannel readableByteChannel = Channels.newChannel(fetchWebsite.openStream());
-        try (FileOutputStream fos = new FileOutputStream("C:\\FreeRam\\jdk-18_windows-x64_bin.exe")) {
-            fos.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
-        Process process = Runtime.getRuntime().exec("cmd /c jdk-18_windows-x64_bin.exe");
-        */
-            
+        
         URL fetchWebsite = new URL(
                 "https://download.sysinternals.com/files/RAMMap.zip"
             );
